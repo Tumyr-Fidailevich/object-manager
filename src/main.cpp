410c47316b1c8c4d494ffd43e35982902a5c8795
Timur <timurkashapov15@gmail.com>
2024-03-01 19:34:37 +0300

    add resources in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QResource>
+
 
 int main(int argc, char *argv[])
 {
+    QResource::registerResource(":/resources.qrc");
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
