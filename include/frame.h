f9aa76d631cd585bdbef7e7933fbc124fa6470d8
Timur <timurkashapov15@gmail.com>
2024-03-01 17:46:13 +0300

    add constructor for custom frame object

--- include/frame.h
+++ include/frame.h
@@ -2,15 +2,36 @@
 #define FRAME_H
 
 #include <QFrame>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QComboBox>
+#include <QSpacerItem>
+#include <QLineEdit>
 
 class Frame : QFrame
 {
     Q_OBJECT
 
 public:
-
+    Frame(QWidget* parent = nullptr);
+signals:
+    void deleteSelf();
 private:
+    QPushButton* _buttonAddProperty;
+    QPushButton* _buttonRemoveProperty;
+    QPushButton* _buttonDeleteFrame;
 
+    QComboBox* _comboBox;
+
+    QVBoxLayout* _mainVLayout;
+    QHBoxLayout* _mainHLayout;
+    QVBoxLayout* _propertyVLayout;
+    QVBoxLayout* _descriptionVLayout;
+    QHBoxLayout* _descriptionHLayout;
+    QHBoxLayout* _buttonsHLayout;
+    QHBoxLayout* _deleteButtonHLayout;
+    QSpacerItem* _deleteButtonHSpacer;
 };
 
 #endif // FRAME_H
--- src/frame.cpp
+++ src/frame.cpp
@@ -1,1 +1,50 @@
 #include "frame.h"
+
+Frame::Frame(QWidget* parent) : QFrame(parent)
+{
+    this->setFrameShape(QFrame::StyledPanel);
+    this->setFrameShadow(QFrame::Raised);
+
+    _mainVLayout = new QVBoxLayout(this);
+    _mainHLayout = new QHBoxLayout();
+    _deleteButtonHLayout = new QHBoxLayout();
+    _mainVLayout->addLayout(_mainHLayout);
+    _mainVLayout->addLayout(_deleteButtonHLayout);
+
+
+    //Delete button
+    _deleteButtonHSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+    _buttonDeleteFrame = new QPushButton();
+    _deleteButtonHLayout->addItem(_deleteButtonHSpacer);
+    _deleteButtonHLayout->addWidget(_buttonDeleteFrame);
+
+
+    //ComboBox
+
+    _comboBox = new QComboBox();
+    _mainHLayout->addWidget(_comboBox);
+
+    //PropertyLayout
+    _propertyVLayout = new QVBoxLayout();
+    _mainHLayout->addLayout(_propertyVLayout);
+    QLineEdit* propertyLineEdit = new QLineEdit();
+    _propertyVLayout->addWidget(propertyLineEdit);
+
+    _buttonAddProperty = new QPushButton();
+    _buttonRemoveProperty = new QPushButton();
+    _buttonsHLayout = new QHBoxLayout();
+    _propertyVLayout->addLayout(_buttonsHLayout);
+    _buttonsHLayout->addWidget(_buttonAddProperty);
+    _buttonsHLayout->addWidget(_buttonRemoveProperty);
+
+    //DesccriptionLayout
+    _descriptionVLayout = new QVBoxLayout();
+    _mainHLayout->addLayout(_descriptionVLayout);
+    QLineEdit* descriptionLineEdit = new QLineEdit();
+    _descriptionVLayout->addWidget(descriptionLineEdit);
+
+    _descriptionHLayout = new QHBoxLayout();
+    _descriptionVLayout->addLayout(_descriptionHLayout);
+
+}
+
